66d5460fe9edfda7f824235d1d8667fc8a6c3e6a
Spencer Attick <spencer.attick@gmail.com>
2024-04-20 06:02:45 -0400

    add and loop through basic blog preview component

--- components/HeaderBar.js
+++ components/HeaderBar.js
@@ -1,16 +1,16 @@
 export default function HeaderBar() {
     return (
       <main className="border border-red-500 p-4">
-        <div class="flex justify-between">
-        <div class="flex">
-            <div class="mr-4">Logo</div>
+        <div className="flex justify-between">
+        <div className="flex">
+            <div className="mr-4">Logo</div>
             <div>Search</div>
         </div>
 
-        <div class="flex">
-            <div class="ml-4">Write</div>
-            <div class="ml-4">Notifications</div>
-            <div class="ml-4">Profile</div>
+        <div className="flex">
+            <div className="ml-4">Write</div>
+            <div className="ml-4">Notifications</div>
+            <div className="ml-4">Profile</div>
         </div>
         </div>
       </main>
--- components/MainSection.js
+++ components/MainSection.js
@@ -1,7 +1,15 @@
+import BlogPreview from "./BlogPreview";
+import blogData from "../public/mockData/blogPreviewData.json";
+
 export default function MainSection() {
     return (
         <main className="border border-green-300 flex-1 p-4">
             <h2>MainSection</h2>
+            {
+                blogData.map((i) => {
+                    return <BlogPreview key={i} />
+                })
+            }
         </main>
     )
 }
