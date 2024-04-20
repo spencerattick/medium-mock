e0c30c4c1e6cac10bf7be0e89c04491cbff3f803
Spencer Attick <spencer.attick@gmail.com>
2024-04-20 13:38:07 -0400

    add icons

--- components/BlogPreview.js
+++ components/BlogPreview.js
@@ -10,21 +10,57 @@ async function getBlogImage() {
     return res
   }
 
+  async function getAuthorProfileImage() {
+    let res = '';
+    try {
+      res = await fetch('https://picsum.photos/100')
+
+    } catch (error) {
+        console.error(error);
+    }
+   
+    return res
+  }
+
+
  
   export default async function BlogPreview({ blog }) {
-    const data = await getBlogImage();
-    const imageUrl = data.url;
+    const blogImage = await getBlogImage();
+    const blogImageUrl = blogImage.url;
+
+    const authorData = await getAuthorProfileImage();
+    const authorImageUrl = authorData.url;
     return (
         <main className="border border-green-300 flex-1 p-4 flex items-center">
-            <div className="flex flex-col flex-1">
-                <div className="text-gray-500">{blog.userName}  &middot; {blog.dataOfPublication}</div>
-                <h1 className="font-bold">{blog.title}</h1>
-                <p>{blog.description}</p>
+        <div className="flex flex-col flex-1">
+          <div className="flex items-center space-x-2 text-s"> {/* Added space-x-2 for horizontal spacing */}
+            <img src={authorImageUrl} alt="Author Profile" className="w-5 h-5 rounded-full" />
+            <span className="text-gray-500">{blog.userName}</span> {/* Wrapped in span for inline styling */}
+            <span className="text-gray-500">&middot;</span>
+            <span className="text-gray-500">{blog.dataOfPublication}</span>
+          </div>
+          <h1 className="font-bold">{blog.title}</h1>
+          <p>{blog.description}</p>
+          <div className="flex items-center space-x-2 text-xs"> {/* Apply space-x-2 for horizontal spacing */}
+            <span className="bg-gray-100 rounded-2xl p-2">{blog.topTag}</span>
+            <span>{blog.length}</span>
+            <div className="ml-auto flex items-center space-x-2"> {/* Adjusted container for tag, read time, and icons */}
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+              </svg>
             </div>
-            <div className="ml-4">
-                <img src={imageUrl} alt="Blog Image" className="w-24 h-24" /> 
-            </div>
-        </main>
+          </div>
+        </div>
+        <div className="ml-4">
+          <img src={blogImageUrl} alt="Blog Image" className="w-24 h-24" /> 
+        </div>
+      </main>
     );
 }
 
--- components/HeaderBar.js
+++ components/HeaderBar.js
@@ -1,16 +1,40 @@
-export default function HeaderBar() {
+async function getProfilePhoto() {
+    let res = '';
+    try {
+      res = await fetch('https://picsum.photos/100')
+
+    } catch (error) {
+        console.error(error);
+    }
+   
+    return res
+  }
+
+export default async function HeaderBar() {
+    const profilePhoto = await getProfilePhoto();
+    const profilePhotoUrl = profilePhoto.url;
     return (
-      <main className="border border-red-500 p-4">
+      <main className="p-4">
         <div className="flex justify-between">
         <div className="flex">
             <div className="mr-4">Logo</div>
-            <div>Search</div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="text-gray-400 w-5 h-5">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            </svg>
+            <input type="text" placeholder="Search" className="ml-4" />
         </div>
 
         <div className="flex">
-            <div className="ml-4">Write</div>
-            <div className="ml-4">Notifications</div>
-            <div className="ml-4">Profile</div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-gray-400">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+            </svg>
+
+            <div className="ml-4 text-gray-400">Write</div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-gray-400">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
+            </svg>
+
+            <img src={profilePhotoUrl} alt="Profile" className="rounded-full w-9 h-9" />
         </div>
         </div>
       </main>
