beffa95afce0a01b6633818f1b83ed190fc0639f
Spencer Attick <spencer.attick@gmail.com>
2024-04-20 07:17:28 -0400

    begin styling BlogPreview

--- components/BlogPreview.js
+++ components/BlogPreview.js
@@ -10,14 +10,23 @@ async function getBlogImage() {
     return res
   }
 
-export default async function BlogPreview({ blog }) {
+ 
+  export default async function BlogPreview({ blog }) {
     const data = await getBlogImage();
     const imageUrl = data.url;
     return (
-        <main className="border border-green-300 flex-1 p-4">
-            <h1 className="font-bold">{blog.title}</h1>
-            <img src={imageUrl} alt="Blog Image" />
-            <p>{blog.description}</p>
+        <main className="border border-green-300 flex-1 p-4 flex items-center">
+            <div className="flex flex-col flex-1">
+                <div className="text-gray-500">{blog.userName}  &middot; {blog.dataOfPublication}</div>
+                <h1 className="font-bold">{blog.title}</h1>
+                <p>{blog.description}</p>
+            </div>
+            <div className="ml-4">
+                <img src={imageUrl} alt="Blog Image" className="w-24 h-24" /> 
+            </div>
         </main>
     );
 }
+
+
+
--- components/RightSideBar.js
+++ components/RightSideBar.js
@@ -1,6 +1,6 @@
 export default function RightSideBar() {
     return (
-        <main className="border border-blue-500 flex-none p-10">
+        <main className="border border-blue-500 flex-none p-5">
             <h2>RightSideBar</h2>
         </main>
     )
