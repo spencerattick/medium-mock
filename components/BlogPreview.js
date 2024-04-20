df8ecbc18e259d362109e4eaab5cff227d5460e9
Spencer Attick <spencer.attick@gmail.com>
2024-04-20 07:00:27 -0400

    successfully retrieve blog image

--- components/BlogPreview.js
+++ components/BlogPreview.js
@@ -1,7 +1,23 @@
-export default function BlogPreview() {
+async function getBlogImage() {
+    let res = '';
+    try {
+      res = await fetch('https://picsum.photos/200')
+
+    } catch (error) {
+        console.error(error);
+    }
+   
+    return res
+  }
+
+export default async function BlogPreview({ blog }) {
+    const data = await getBlogImage();
+    const imageUrl = data.url;
     return (
         <main className="border border-green-300 flex-1 p-4">
-            <h2>BlogPreview</h2>
+            <h1 className="font-bold">{blog.title}</h1>
+            <img src={imageUrl} alt="Blog Image" />
+            <p>{blog.description}</p>
         </main>
-    )
+    );
 }
--- components/MainSection.js
+++ components/MainSection.js
@@ -1,14 +1,13 @@
 import BlogPreview from "./BlogPreview";
 import blogData from "../public/mockData/blogPreviewData.json";
 
-export default function MainSection() {
+export default function MainSection({ imageUrl }) {
     return (
         <main className="border border-green-300 flex-1 p-4">
-            <h2>MainSection</h2>
             {
-                blogData.map((i) => {
-                    return <BlogPreview key={i} />
-                })
+                blogData.map((blog, index) => (
+                    <BlogPreview key={index} blog={blog} />
+                ))
             }
         </main>
     )
